8572d745426d1904497570ab6e90226b1e2ce238
Krystian Gaj <krystian0120@outlook.com>
2021-04-28 00:03:07 +0200

    Exercise 2 - controllers - WIP

---
+++ play_sklep/sklep/app/controllers/PartCategoryController.scala
@@ -0,0 +1,9 @@
+package controllers
+
+class PartCategoryController {
+  def getPartCategory() {}
+  def getAllPartCategories() {}
+  def createPartCategory() {}
+  def updatePartCategory() {}
+  def deletePartCategory() {}
+}
